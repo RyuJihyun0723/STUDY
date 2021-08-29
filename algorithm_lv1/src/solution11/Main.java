7596f3c7f5c71f24a32664f87c753ccd4fe8e56c
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 20:07:38 +0900

    서울에서 김서방 찾기

---
+++ algorithm_lv1/src/solution11/Main.java
@@ -0,0 +1,13 @@
+package solution11;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+
+        Solution solution = new Solution();
+
+        String result = solution.solution(seoul);
+
+        System.out.println(result);
+    }
+}
