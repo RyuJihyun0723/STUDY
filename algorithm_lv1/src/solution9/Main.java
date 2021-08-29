b0dd0afd3efdafad00d5758acd95fec506d24710
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 19:47:30 +0900

    문자열 내 p와 y의 개수

---
+++ algorithm_lv1/src/solution9/Main.java
@@ -0,0 +1,13 @@
+package solution9;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+
+        Solution solution = new Solution();
+
+        boolean result = solution.solution(s);
+
+        System.out.println(result);
+    }
+}
