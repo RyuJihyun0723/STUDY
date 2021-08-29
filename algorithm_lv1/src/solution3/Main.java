3593d0ea60f18ccfe591527aa47e9ab4864397bc
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 18:33:55 +0900

    가운데 글자 가져오기

---
+++ algorithm_lv1/src/solution3/Main.java
@@ -0,0 +1,13 @@
+package solution3;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "abcde";
+
+        Solution solution = new Solution();
+
+        String result = solution.solution(s);
+
+        System.out.println(result);
+    }
+}
