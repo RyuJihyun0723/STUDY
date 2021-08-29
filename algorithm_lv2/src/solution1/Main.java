2e062deea9a9d69a18b36b62c0499a7e7527d06a
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 19:57:08 +0900

    최댓값과 최솟값

---
+++ algorithm_lv2/src/solution1/Main.java
@@ -0,0 +1,13 @@
+package solution1;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "1 2 3 4";
+
+        Solution solution = new Solution();
+
+        String result = solution.solution(s);
+
+        System.out.println(result);
+    }
+}
