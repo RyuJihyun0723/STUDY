b6d839eeec0c42c19d9b4157821fd5253a6088aa
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 19:48:57 +0900

    문자열 다루기 기본

---
+++ algorithm_lv1/src/solution10/Main.java
@@ -0,0 +1,13 @@
+package solution10;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "a234";
+
+        Solution solution = new Solution();
+
+        boolean result = solution.solution(s);
+
+        System.out.println(result);
+    }
+}
