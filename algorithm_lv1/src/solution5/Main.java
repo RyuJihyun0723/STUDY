5a9a7411df6fb79d0b3298da3e89d701779ee125
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 19:38:25 +0900

    문자열을 정수로 바꾸기

---
+++ algorithm_lv1/src/solution5/Main.java
@@ -0,0 +1,13 @@
+package solution5;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "1234";
+
+        Solution solution = new Solution();
+
+        int result = solution.solution(s);
+
+        System.out.println(result);     
+    }
+}
