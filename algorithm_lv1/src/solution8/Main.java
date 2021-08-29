d2103f7d266863a83118e86c66b86e0b798f2df4
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 19:45:44 +0900

    두 정수 사이의 합

---
+++ algorithm_lv1/src/solution8/Main.java
@@ -0,0 +1,14 @@
+package solution8;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        Solution solution = new Solution();
+
+        long result = solution.solution(a, b);
+
+        System.out.println(result);
+    }
+}
---
+++ algorithm_lv1/src/solution8/Solution.java
@@ -0,0 +1,21 @@
+package solution8;
+
+class Solution {
+    public long solution(int a, int b) {
+       long answer = 0;
+      
+       if(a > b) {
+           int temp = a;
+           a = b;
+           b = temp;
+       }
+       if(a != b) {
+           for(int i = a; i <= b; i++) {
+               answer += i;
+           }
+       }else {
+           answer = a;
+       }
+       return answer;
+   }
+}
