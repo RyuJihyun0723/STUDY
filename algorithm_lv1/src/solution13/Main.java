98b44fd57d01250c6fdb55037e674bd8636ffae8
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 20:10:33 +0900

    수박수박수박수박수박수?

---
+++ algorithm_lv1/src/solution13/Main.java
@@ -0,0 +1,13 @@
+package solution13;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+
+        Solution solution = new Solution();
+
+        String result = solution.solution(n);
+
+        System.out.println(result);
+    }
+}
