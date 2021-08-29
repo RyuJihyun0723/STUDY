689021c77e70306394ccc466b368722d927d0634
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-29 13:12:09 +0900

    2016년

---
+++ algorithm/src/year2016/Main.java
@@ -0,0 +1,14 @@
+package algorithm.src.year2016;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 24;
+
+        Solution s = new Solution();
+
+        String result = s.solution(a, b);
+
+        System.out.println(result);
+    }
+}
