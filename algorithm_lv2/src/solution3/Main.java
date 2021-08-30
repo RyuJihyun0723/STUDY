c9922268a7b13e8328513e63af1026261c76974f
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-30 20:02:37 +0900

    다음 큰 수

---
+++ algorithm_lv2/src/solution3/Main.java
@@ -0,0 +1,13 @@
+package solution3;
+
+public class Main{
+    public static void main(String[] args) {
+        int n = 78;
+
+        Solution solution = new Solution();
+
+        int result = solution.solution(n);
+
+        System.out.println(result);
+    }
+}
