3d67c18059e4f2327ae0a3c61522f08920526e23
RyuJihyun0723 <qaer028694@gmail.com>
2021-08-30 13:02:31 +0900

    124나라의 숫자

---
+++ algorithm_lv2/src/solution1/Main.java
@@ -0,0 +1,13 @@
+package solution1;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 1; 
+
+        Solution solution = new Solution();
+
+        String result = solution.solution(n);
+
+        System.out.println(result);
+    }
+}
