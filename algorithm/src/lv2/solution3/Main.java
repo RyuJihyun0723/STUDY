478a136d5670d7c67d77f50b673ca855c9a47391
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-02 00:21:49 +0900

    숫자의 표현

---
+++ algorithm/src/lv2/solution3/Main.java
@@ -0,0 +1,13 @@
+package lv2.solution3;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 15;
+
+        Solution s = new Solution();
+
+        int result = s.solution(n);
+
+        System.out.println(result);
+    }
+}
