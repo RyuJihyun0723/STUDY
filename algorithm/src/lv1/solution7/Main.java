4f2502b1821f385cbcb4fa27c2f50765ef713715
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:18:58 +0900

    2016년

---
+++ algorithm/src/lv1/solution7/Main.java
@@ -0,0 +1,13 @@
+package lv1.solution7;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 24;
+
+        Solution s = new Solution();
+
+        String result = s.solution(a, b);
+        System.out.println(result);
+    }
+}
---
+++ algorithm/src/lv1/solution7/Solution.java
@@ -0,0 +1,21 @@
+package lv1.solution7;
+
+class Solution {
+    String[] weeks = { "FRI","SAT","SUN","MON","TUE","WED","THU"};
+	int[] month = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+	
+	public String solution(int a, int b) {
+        String answer = "";
+        	
+        int day = 0;
+        	
+        for (int k = 0; k <(a-1); k++) {
+        		day += month[k];
+        }
+        day += (b-1);
+        day %= 7;
+        answer = weeks[day];
+
+        return answer;
+    }
+}
