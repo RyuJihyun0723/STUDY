c5747653796d97ddb21d74335125dd71dd9bde37
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:23:45 +0900

    나누어 떨어지는 숫자 배열

---
+++ algorithm/src/lv1/solution10/Main.java
@@ -0,0 +1,11 @@
+package lv1.solution10;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(arr, divisor));
+    }
+}
