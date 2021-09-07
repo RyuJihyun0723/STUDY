66d5439d5f396d36f51292bc3a81d5cb9bb8eabf
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 16:36:24 +0900

    다음 큰 숫자

---
+++ algorithm/src/lv2/solution7/Main.java
@@ -0,0 +1,11 @@
+package lv2.solution7;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 78;
+        Solution s = new Solution();
+
+        int result = s.solution(n);
+        System.out.println(result);
+    }
+}
