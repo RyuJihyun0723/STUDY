acef3dd25f31815dcc7485115ede022c2a71b0f1
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:31:23 +0900

    문자열 내 p와 y의 개수

---
+++ algorithm/src/lv1/solution12/Main.java
@@ -0,0 +1,10 @@
+package lv1.solution12;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(s));
+    }
+}
---
+++ algorithm/src/lv1/solution12/Solution.java
@@ -0,0 +1,30 @@
+package lv1.solution12;
+
+class Solution {
+	boolean solution(String s) {
+        boolean answer = true;
+        s = s.toUpperCase();
+        
+        String[] arr; 
+        
+        arr = s.split(""); 
+        int a = 0;
+        int b = 0;
+        
+        for(int i=0;i<arr.length;i++) { 
+        	if(arr[i].equals("P")) {
+        		a++;
+        	}else if(arr[i].equals("Y")) {
+        		b++;
+        	}
+        }
+        if(a == b) {
+        	answer = true;
+        }else {
+        	answer = false;
+        }
+        
+
+        return answer;
+    }
+}
