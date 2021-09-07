ca111a2ce53c47923c42af39c9d9e73770662b6b
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 21:15:46 +0900

    핸드폰 번호 가리기

---
+++ algorithm/src/lv1/solution28/Main.java
@@ -0,0 +1,9 @@
+package lv1.solution28;
+
+public class Main {
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        Solution s = new Solution();
+        System.out.println(s.solution(phone_number));
+    }
+}
