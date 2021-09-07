1c758803fd677e2512a69723ab6312b74cad8354
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 16:33:27 +0900

    올바른 괄호

---
+++ algorithm/src/lv2/solution6/Main.java
@@ -0,0 +1,11 @@
+package lv2.solution6;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "()()";
+        Solution sol = new Solution();
+
+        boolean result = sol.solution(s);
+        System.out.println(result);
+    }
+}
