80502548b53ed6817782c076fed8298da74f3ab2
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:46:29 +0900

    소수 찾기

---
+++ algorithm/src/lv1/solution15/Main.java
@@ -0,0 +1,9 @@
+package lv1.solution15;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        Solution s = new Solution();
+        System.out.println(s.solution(n));
+    }
+}
