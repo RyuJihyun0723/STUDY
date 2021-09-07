04bf4bfb42ddcf31a2b41f829eb273b028dbb6f0
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 16:49:07 +0900

    내적

---
+++ algorithm/src/lv1/solution4/Main.java
@@ -0,0 +1,14 @@
+package lv1.solution4;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+
+        Solution s = new Solution();
+
+        int result = s.solution(a, b);
+
+        System.out.println(result);
+    }
+}
