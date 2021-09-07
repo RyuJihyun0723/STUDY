cc40b348c907682ad36f5e916552dab5fae26267
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:55:44 +0900

    이상한 문자 만들기

---
+++ algorithm/src/lv1/solution20/Main.java
@@ -0,0 +1,10 @@
+package lv1.solution20;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "try hello world";
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(s));
+    }
+}
---
+++ algorithm/src/lv1/solution20/Solution.java
@@ -0,0 +1,26 @@
+package lv1.solution20;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        s = s.toLowerCase();
+        
+        String[] arr = s.split(" ", -1);
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length(); j++){
+                String[] arr2 = arr[i].split("");
+                if(j % 2 == 0){
+                    arr2[j] = arr2[j].toUpperCase();
+                }
+                answer += arr2[j];
+            }
+            if(i == arr.length-1){
+                break;
+            }else{
+                answer += " ";
+            }
+        }
+        return answer;
+    }
+}
