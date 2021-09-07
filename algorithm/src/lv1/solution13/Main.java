cdf18c98b17e88b37e2978830f8dc0addbfc8f21
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:32:18 +0900

    문자열 다루기 기본

---
+++ algorithm/src/lv1/solution13/Main.java
@@ -0,0 +1,10 @@
+package lv1.solution13;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "a234";
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(s));
+    }
+}
---
+++ algorithm/src/lv1/solution13/Solution.java
@@ -0,0 +1,20 @@
+package lv1.solution13;
+
+class Solution {
+    public boolean solution(String s) {
+    boolean answer = true;
+               int i = s.length();
+               s = s.replaceAll("[a-zA-Z]", "");
+               
+              if(i == 4 || i == 6 ) {
+                  if(s.length() < i){
+                      answer = false;
+                  }else if(s.length() == i){
+                       answer = true;   
+                  }
+               }else {
+                  answer = false;
+              }
+               return answer;
+           }
+       }
