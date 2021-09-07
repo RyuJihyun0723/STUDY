7f1bc6500c3f308cb669655e43a99ace60623451
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:50:04 +0900

    시저 암호

---
+++ algorithm/src/lv1/solution18/Main.java
@@ -0,0 +1,11 @@
+package lv1.solution18;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "AB";
+        int n = 1;
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(s, n));
+    }
+}
