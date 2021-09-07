542b4687d9d9462d66f00930a5032048be994c5c
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:50:52 +0900

    약수의 합

---
+++ algorithm/src/lv1/solution19/Main.java
@@ -0,0 +1,10 @@
+package lv1.solution19;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 12;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(n));
+    }
+}
