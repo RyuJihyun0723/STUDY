85db7f2d91007f1a4b0a7c333fafa96f862f4616
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:30:06 +0900

    두 정수 사이의 합

---
+++ algorithm/src/lv1/solution11/Main.java
@@ -0,0 +1,11 @@
+package lv1.solution11;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(a, b));
+    }
+}
