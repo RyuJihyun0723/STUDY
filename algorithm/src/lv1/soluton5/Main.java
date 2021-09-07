8389be5172c7a8357d7168dce53ac7bf3d95a789
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 16:50:37 +0900

    약수의 개수와 덧셈

---
+++ algorithm/src/lv1/soluton5/Main.java
@@ -0,0 +1,14 @@
+package lv1.soluton5;
+
+public class Main {
+    public static void main(String[] args) {
+        int left = 13;
+        int right = 17;
+        
+        Solution s = new Solution();
+
+        int result = s.solution(left, right);
+
+        System.out.println(result);
+    }
+}
