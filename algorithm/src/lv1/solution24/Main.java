2964371d8a5697c011b9ea2af694272ffb56f97a
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 21:11:47 +0900

    짝수와 홀수

---
+++ algorithm/src/lv1/solution24/Main.java
@@ -0,0 +1,9 @@
+package lv1.solution24;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 3;
+        Solution s = new Solution();
+        System.out.println(s.solution(num));
+    }
+}
