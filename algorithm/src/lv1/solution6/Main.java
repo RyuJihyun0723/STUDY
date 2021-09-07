711ab96e0248d0b0f6c15199ce40ff16a4ac3c65
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 20:14:49 +0900

    예산

---
+++ algorithm/src/lv1/solution6/Main.java
@@ -0,0 +1,13 @@
+package lv1.solution6;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] d = {1, 3, 2, 5, 4};
+        int budget = 9;
+
+        Solution s = new Solution();
+
+        int result = s.solution(d, budget);
+        System.out.println(result);
+    }
+}
