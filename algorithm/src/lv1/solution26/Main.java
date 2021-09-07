0fc94a573d2a6617d9ec392eaa5eeee6971b2e21
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 21:13:47 +0900

    평균 구하기

---
+++ algorithm/src/lv1/solution26/Main.java
@@ -0,0 +1,9 @@
+package lv1.solution26;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        Solution s = new Solution();
+        System.out.println(s.solution(arr));
+    }
+}
