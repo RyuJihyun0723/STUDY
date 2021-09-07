040d97702c858be525689d2d679086f7db9f87e0
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 21:09:38 +0900

    정수 제곱근 판별

---
+++ algorithm/src/lv1/solution22/Main.java
@@ -0,0 +1,9 @@
+package lv1.solution22;
+
+public class Main {
+    public static void main(String[] args) {
+        long n = 121;
+        Solution s = new Solution();
+        System.out.println(s.solution(n));
+    }
+}
