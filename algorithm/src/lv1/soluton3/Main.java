28d82abe4cefafb3490eb7443b568809883058a2
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-07 16:46:44 +0900

    음양 더하기

---
+++ algorithm/src/lv1/soluton3/Main.java
@@ -0,0 +1,14 @@
+package lv1.soluton3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true, false, true};
+
+        Solution s = new Solution();
+
+        int result = s.solution(absolutes, signs);
+
+        System.out.println(result);
+    }
+}
