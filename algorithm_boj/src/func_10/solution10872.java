3eb36787d78fb5ba59eb584a9bc20eeb8f4ece15
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-11 12:28:51 +0900

    팩토리얼

---
+++ algorithm_boj/src/func_10/solution10872.java
@@ -0,0 +1,21 @@
+package func_10;
+
+import java.util.Scanner;
+
+public class solution10872 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int N){
+        if(N == 0){
+            return 1;
+        }else{
+            return N * factorial(N-1);
+        }
+    }
+}
