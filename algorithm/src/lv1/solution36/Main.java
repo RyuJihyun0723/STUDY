be5faba43bffb1cd3c6b1f2d4d6e83d47d2b2181
RyuJihyun0723 <qaer028694@gmail.com>
2021-09-14 23:32:11 +0900

    부족한 금액 계산하기

---
+++ algorithm/src/lv1/solution36/Main.java
@@ -0,0 +1,12 @@
+package solution36;
+
+public class Main {
+    public static void main(String[] args) {
+        long price = 3;
+        long money = 20;
+        long count = 4;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(price, money, count));
+    }
+}
---
+++ algorithm/src/lv1/solution36/Solution.java
@@ -0,0 +1,19 @@
+package solution36;
+
+class Solution {
+	public long solution(long price, long money, long count) {
+	        long answer = -1;
+	        
+	        for(long i = 0; i <= count; i++) {
+	        	money -= (price*i);
+	        }
+        
+	       if(money >= 0) {
+	    	   answer = 0;
+	       }else if(money < 0) {
+	    	   answer = Math.abs(money);
+	       }
+	        
+	        return answer;
+	    }
+}
