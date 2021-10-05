38f6bd75b103e25b2ee166b995ff49118f5ba83f
RyuJihyun0723 <qaer028694@gmail.com>
2021-10-05 23:26:44 +0900

    A + B - 5

--- baekjoon/src/solution10952.java
+++ baekjoon/src/solution10952.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class solution10952 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
 
         while(true){
             int A = scan.nextInt();
@@ -12,14 +10,11 @@ public class solution10952 {
             if(A == 0 && B == 0){
                 break;
             }else{
-                arr.add(A+B);
+                System.out.println(A+B);
             }
         }
 
         scan.close();
 
-        for(int i = 0; i < arr.size(); i++){
-            System.out.println(arr.get(i));
-        }
     }
 }
