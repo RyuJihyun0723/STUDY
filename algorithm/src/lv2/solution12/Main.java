62f94b0a401a246122d985042f3a7d99dc55ed23
RyuJihyun0723 <qaer028694@gmail.com>
2021-10-13 11:35:24 +0900

    전화번호 목록

---
+++ algorithm/src/lv2/solution12/Main.java
@@ -0,0 +1,10 @@
+package lv2.solution12;
+
+public class Main{
+    public static void main(String[] args) {
+        String[] phone_book = {"123","456","789"};
+
+        Solution s = new Solution();
+        System.out.println(s.solution(phone_book));
+    }
+}
