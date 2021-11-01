216ecfcace3a613933dbbbbb33c6a333798329d9
RyuJihyun0723 <qaer028694@gmail.com>
2021-11-02 08:47:57 +0900

    수 찾기

---
+++ baekjoon/src/solution1920.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class solution1920 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int[] arr1 = new int[N];
+        for(int i = 0; i < N; i++){
+            arr1[i] = scan.nextInt();
+        }
+        int M = scan.nextInt();
+        int[] arr2 = new int[M];
+        for(int i = 0; i < M; i++){
+            arr2[i] = scan.nextInt();
+        }
+
+        scan.close();
+        int result = 0;
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(arr2[i] == arr1[j]){
+                    result = 1;
+                }
+            }
+            System.out.println(result);
+            result = 0;
+        }
+    }
+}
